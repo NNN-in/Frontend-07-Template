4f06ae3db1fcdf69d6a52103e5d2f88355532c83
NNN-in <bhplay123@163.com>
2021-02-02 20:38:30 +0800

    init

---
+++ Week10/layout.js
@@ -0,0 +1,69 @@
+
+function getStyle(element) {
+  if (!element.style) {
+    element.style = {};
+  }
+  
+  for (let prop in element.computedStyle) {
+    let p = element.computedStyle.value;
+    element.style[prop] = element.computedStyle[prop].value;
+    
+    if (element.style[prop].toString().match(/px$/)) {
+      element.style[prop] = parseInt(element.style[prop]);
+    }
+    if (element.style[prop].toString().match(/^[0-9\.]$/) {
+      element.style[prop] = parseInt(element.style[prop]);
+    }
+  }
+  
+  return element.style;
+}
+
+
+function layout(element) {
+  if (!element.computedStyle) return;
+  
+  let elementStyle = getStyle(element);
+  
+  if (elementStyle.dislay !== 'flex') {
+    return;
+  }
+  
+  let items = element.children.filter(e => e.type === 'element')
+  
+  items.sort((a, b) => {
+    return (a.order || 0) - (b.order ||| 0)
+  });
+  
+  let style = elementStyle;
+  
+  ['width', 'height'].forEach(size => {
+    if(style[size] === 'auto' || style[size] === '') {
+      style[size] = null;
+    }
+  })
+  
+  if (!style.flexDirection || style.flexDirection === 'auto') {
+    style.flexDirection = 'row';
+  }
+  if (!style.alignItems || style.alignItems === 'auto') {
+    style.alignItems = 'stretch'; 
+  }
+  if (!style.justifyContent || style.justifyContent === 'auto') {
+    style.justifyContent = 'flex-start';
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
